97501783c35bb68589d7d2c59cd2f8aefb6c63a5
Gavin Dhaliwal <gavindhaliwal123@gmail.com>
2024-06-05 14:38:52 -0400

    Fixed bugs to do with page handling

--- server.py
+++ server.py
@@ -5,55 +5,6 @@
 from http.server import BaseHTTPRequestHandler,HTTPServer
 import os
 
-"Each one of these classes implement a test and action, action to be executed if the test is true"
-class case_no_file(object):
-    '''File or directory does not exist'''
-    def test(self, handler):
-        return not os.path.exists(handler.full_path)
-    
-    def act(self, handler):
-        raise ServerException(f"{handler.path} not found") # what is this handler?
-    
-class case_existing_file(object):
-    '''Case of file existing'''
-    def test(self, handler):
-        return os.path.isfile(handler.full_path)
-    
-    def act(self, handler):
-        handler.handle_file(handler.full_path)
-
-class case_always_fail(object):
-    '''Case of file existing'''
-    def test(self, handler):
-        True
-    
-    def act(self, handler):
-        raise ServerException(f"Unknown object: {handler.full_path}")
-
-class case_directory_index_file(object):
-    '''In case there is an index file in a certain directory, serve that index.html'''
-    def index_path(self, handler):
-        # Function that just joins together to form a potential path to a potential index.html
-        return os.path.join(handler.full_path, 'index.html')
-
-    def test(self, handler):
-        return os.path.isdir(handler.full_path) and os.path.isfile(self.index_path(handler))
-    
-    def act(self, handler):
-        handler.handle_file(self.index_path(handler))
-
-class case_directory_no_index_file(object):
-    '''Directory path, index file does not exist, action is to show listing'''
-    def index_path(self, handler):
-        # Function that just joins together to form a potential path to a potential index.html
-        return os.path.join(handler.full_path, 'index.html')
-
-    def test(self, handler):
-        return os.path.isdir(handler.full_path) and not os.path.isfile(self.index_path(handler))
-    
-    def act(self, handler):
-        '''We want to list directory contents'''
-
 class RequestHandler(BaseHTTPRequestHandler):
     ''' Serving a basic page back to the user '''
 
@@ -90,11 +41,62 @@ class RequestHandler(BaseHTTPRequestHandler):
         </html>
         """
     
+    "Each one of these classes implement a test and action, action to be executed if the test is true"
+    class case_no_file(object):
+        '''File or directory does not exist'''
+        def test(self, handler):
+            return not os.path.exists(handler.full_path)
+        
+        def act(self, handler):
+            raise ServerException(f"{handler.path} not found")
+        
+    class case_existing_file(object):
+        '''Case of file existing'''
+        def test(self, handler):
+            return os.path.isfile(handler.full_path)
+        
+        def act(self, handler):
+            handler.handle_file(handler.full_path)
+
+    class case_always_fail(object):
+        '''Case of file existing'''
+        def test(self, handler):
+            True
+        
+        def act(self, handler):
+            raise ServerException(f"Unknown object: {handler.full_path}")
+
+    class case_directory_index_file(object):
+        '''In case there is an index file in a certain directory, serve that index.html'''
+        def index_path(self, handler):
+            # Function that just joins together to form a potential path to a potential index.html
+            return os.path.join(handler.full_path, 'index.html')
+
+        def test(self, handler):
+            return os.path.isdir(handler.full_path) and os.path.isfile(self.index_path(handler))
+        
+        def act(self, handler):
+            handler.handle_file(self.index_path(handler))
+
+    class case_directory_no_index_file(object):
+        '''Directory path, index file does not exist, action is to show listing'''
+        def index_path(self, handler):
+            # Function that just joins together to form a potential path to a potential index.html
+            return os.path.join(handler.full_path, 'index.html')
+
+        def test(self, handler):
+            return os.path.isdir(handler.full_path) and not os.path.isfile(self.index_path(handler))
+        
+        def act(self, handler):
+            '''We want to list directory contents'''
+            handler.show_listing_page(handler.full_path)
+
+    
     def show_listing_page(self, full_path):
         '''Function shows the listing page, called in case a directory is accessed with no index.html file'''
         try:
             entries = os.listdir(full_path)
-            bullets = ["<li>{e}</li>" for e in entries if not e.startswith(".")] # list comprehension to fill <li></li> elements
+            bullets = [f"<li>{e}</li>" for e in entries if not e.startswith(".")] # list comprehension to fill <li></li> elements
             page = self.Listing_Page.format('\n'.join(bullets)) # bullets is a list, join at a newline
             self.send_content(page)
         except OSError as msg:
@@ -103,7 +105,7 @@ class RequestHandler(BaseHTTPRequestHandler):
 
     
     # This acts like an enum
-    Cases = [case_no_file(), case_existing_file(), case_directory_index_file(), case_always_fail()]
+    Cases = [case_no_file(), case_existing_file(), case_directory_index_file(), case_directory_no_index_file(), case_always_fail()]
     def create_page(self):
         values = {
             # These are either standard Python functions or defined as instance variables for the BaseHTTPRequestHandler class
@@ -122,7 +124,7 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.send_header("Content-Length", str(len(content)))
         self.end_headers()
         # self.wfile.write(bytes(str(content), encoding="utf8")) # must be sent back as bytes
-        self.wfile.write(content)
+        self.wfile.write(content.encode())
 
     # serve a GET request
     def do_GET(self):
@@ -130,25 +132,14 @@ class RequestHandler(BaseHTTPRequestHandler):
         # self.send_page(page)
         try:
             # figure out exactly what is being requested
-            full_path = os.getcwd() + self.path
-            print(full_path)
+            self.full_path = os.getcwd() + self.path
 
-            # # If we cannot find this file, we will raise a server exception
-            # if not os.path.exists(full_path):
-            #     raise ServerException(f"{self.path} not found")
-            
-            # # if a file, we handle it
-            # elif os.path.isfile(full_path):
-            #     self.handle_file(full_path)
-            
-            # else:
-            #     raise ServerException(f"Unknown object {self.path}")
             for case in self.Cases:
-                handler = case()
-                # Why do we have to add a self here?
+                handler = case
+                # We add self to add a reference to the RequestHandler object
                 if handler.test(self):
                     handler.act(self)
-                    break
+                    break # breaks out of the for loop
 
         
         except Exception as msg:
@@ -157,7 +148,7 @@ class RequestHandler(BaseHTTPRequestHandler):
     def handle_file(self, full_path):
         ''' Handles a file, opens it, sends content'''
         try:
-            with open(full_path, "rb") as reader:
+            with open(full_path, "r") as reader:
                 content = reader.read()
             self.send_content(content)
         except IOError as msg:
